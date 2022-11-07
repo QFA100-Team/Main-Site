dfc2e6062f4626b0aa2545e12d603c9dee8abd7f
JoshuaMicallefYBSU <joshuamicallef66@gmail.com>
2022-11-07 16:06:34 +1000

    Main Page
    Updated to welcome in 2023.

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -7,7 +7,7 @@
         <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.slim.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js"></script>
-        <title>QFA100 - LANDING SOON</title>
+        <title>QFA100 Team - 2023</title>
 
         <!-- Fonts -->
         <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -15,6 +15,7 @@
 
     <body>
         <!-- Opening Discussion Text (In Grey) -->
+<<<<<<< Updated upstream:resources/views/landingpage.blade.php
         <div style="padding: 20px;" class="jumbotron text-center">
             <img style="align: center; max-width: 30%; padding-bottom: 10px;" src="https://cdn.discordapp.com/attachments/651917468251455497/1005605946623533126/unknown_15.png">
             <h1><b>QFA100 Team Website</b></h1>
@@ -27,6 +28,17 @@
                 In 2022, we are delighted to bring back the QFA100 Team for our third year of operation!<br>
                 The team is looking forward to being involved in WorldFlight 2022!<br><br>
                 Check out the WorldFlight 2022 Route <a href="https://cdn.discordapp.com/attachments/477414012489826324/1005309795806478446/2022_Route_Final.jpg" target="_blank">here</a>!.
+=======
+        <div class="jumbotron text-center">
+            <img style="align: center; max-width: 25%;" src="https://cdn.discordapp.com/attachments/651917468251455497/1005605946623533126/unknown_15.png">
+            <h1>QFA100 Team</h1>
+            <h3>WorldFlight 2023</h3>
+            <p>
+                WorldFlight 2023 is the forth itteration of the QFA100 Unofficial Team!
+                <br><br>Beginning in 2019, Linn M began flying the QFA100 Callsign during WorldFlight 2019.
+                <br><br>During 2020, Linn approached a number of dedicated VATPAC and VATSIM Members with the proposal to fly a group callsign. In 2022, we are delighted to bring the QFA100 Team!
+				<br><br>Unfortunatley, due to commitments with the Leadership Team, QFA100 was not able to be brought into the spotlight for WorldFlight 2022. The team plans to be well prepared for WorldFlight 2023!
+>>>>>>> Stashed changes:resources/views/welcome.blade.php
             </p>
         </div>
 
